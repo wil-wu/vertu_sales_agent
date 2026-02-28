db96e3a92cec2f767871560ee3fedef6dfb5a488
wil-wu <wil-wu@outlook.com>
2026-02-28 17:52:37 +0800

    Add llm temperature

--- app/services/react_agent/shared.py
+++ app/services/react_agent/shared.py
@@ -6,4 +6,5 @@ chat_model = ChatOpenAI(
     base_url=react_agent_settings.openai_base_url,
     api_key=react_agent_settings.openai_api_key,
     model=react_agent_settings.llm_model,
+    temperature=react_agent_settings.temperature,
 )
